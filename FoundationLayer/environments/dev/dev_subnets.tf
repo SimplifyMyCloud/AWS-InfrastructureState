4158a16b6c7d3b77823c6e6ee913762ae7f440ec
chrisL <chris@simplifymy.cloud>
2018-10-11 15:21:32 -0700

    working modueles

--- FoundationLayer/environments/dev/dev_subnets.tf
+++ FoundationLayer/environments/dev/dev_subnets.tf
@@ -7,47 +7,47 @@ module "aws_vpc_subnet_private_a" {
   automation                    =   "terraform"
 }
 
-module "aws_vpc_subnet_private_b" {
-  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
-
-  vpc_private_subnet_name_a     =   "smc-dev-vpc-subnet-priv-b"
-  environment                   =   "development"
-  layer                         =   "foundation"
-  automation                    =   "terraform"
-}
-
-module "aws_vpc_subnet_private_c" {
-  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
-
-  vpc_private_subnet_name_a     =   "smc-dev-vpc-subnet-priv-c"
-  environment                   =   "development"
-  layer                         =   "foundation"
-  automation                    =   "terraform"
-}
-
-module "aws_vpc_subnet_public_a" {
-  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
-
-  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-a"
-  environment                   =   "development"
-  layer                         =   "foundation"
-  automation                    =   "terraform"
-}
-
-module "aws_vpc_subnet_public_b" {
-  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
-
-  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-b"
-  environment                   =   "development"
-  layer                         =   "foundation"
-  automation                    =   "terraform"
-}
-
-module "aws_vpc_subnet_public_c" {
-  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
-
-  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-c"
-  environment                   =   "development"
-  layer                         =   "foundation"
-  automation                    =   "terraform"
-}
+#module "aws_vpc_subnet_private_b" {
+#  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
+#
+#  vpc_private_subnet_name_a     =   "smc-dev-vpc-subnet-priv-b"
+#  environment                   =   "development"
+#  layer                         =   "foundation"
+#  automation                    =   "terraform"
+#}
+#
+#module "aws_vpc_subnet_private_c" {
+#  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
+#
+#  vpc_private_subnet_name_a     =   "smc-dev-vpc-subnet-priv-c"
+#  environment                   =   "development"
+#  layer                         =   "foundation"
+#  automation                    =   "terraform"
+#}
+#
+#module "aws_vpc_subnet_public_a" {
+#  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
+#
+#  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-a"
+#  environment                   =   "development"
+#  layer                         =   "foundation"
+#  automation                    =   "terraform"
+#}
+#
+#module "aws_vpc_subnet_public_b" {
+#  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
+#
+#  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-b"
+#  environment                   =   "development"
+#  layer                         =   "foundation"
+#  automation                    =   "terraform"
+#}
+#
+#module "aws_vpc_subnet_public_c" {
+#  source = "/Users/spudbud/Codestuff/AWS-InfrastructureState/FoundationLayer/modules/aws_vpc_subnets"
+#
+#  vpc_public_subnet_name_a     =   "smc-dev-vpc-subnet-priv-c"
+#  environment                   =   "development"
+#  layer                         =   "foundation"
+#  automation                    =   "terraform"
+#}
--- FoundationLayer/modules/aws_vpc_subnets/vpc_subnets.tf
+++ FoundationLayer/modules/aws_vpc_subnets/vpc_subnets.tf
@@ -6,25 +6,25 @@ variable "vpc_private_subnet_name_a" {
   description   =   "the name of the private subnet for the environment in the A availability zone" 
 }
 
-variable "vpc_private_subnet_name_b" {
-  description   =   "the name of the private subnet for the environment in the B availability zone" 
-}
-
-variable "vpc_private_subnet_name_c" {
-  description   =   "the name of the private subnet for the environment in the C availability zone" 
-}
-
-variable "vpc_public_subnet_name_a" {
-  description   =   "the name of the public subnet for the environment in the A availability zone" 
-}
-
-variable "vpc_public_subnet_name_b" {
-  description   =   "the name of the public subnet for the environment in the B availability zone" 
-}
-
-variable "vpc_public_subnet_name_c" {
-  description   =   "the name of the public subnet for the environment in the C availability zone" 
-}
+#variable "vpc_private_subnet_name_b" {
+#  description   =   "the name of the private subnet for the environment in the B availability zone" 
+#}
+#
+#variable "vpc_private_subnet_name_c" {
+#  description   =   "the name of the private subnet for the environment in the C availability zone" 
+#}
+#
+#variable "vpc_public_subnet_name_a" {
+#  description   =   "the name of the public subnet for the environment in the A availability zone" 
+#}
+#
+#variable "vpc_public_subnet_name_b" {
+#  description   =   "the name of the public subnet for the environment in the B availability zone" 
+#}
+#
+#variable "vpc_public_subnet_name_c" {
+#  description   =   "the name of the public subnet for the environment in the C availability zone" 
+#}
 
 variable  "environment" {
   description   =   "the environment being targeted to ensure the desired state"
@@ -43,69 +43,69 @@ resource    "aws_subnet"    "private_subnet_a"   {
     cidr_block  =   "10.0.10.0/24"
 
     tags         {
-        Name        = "${var.vpc_private_subnet_name_a}"
+        Name        = "test-name-subnet"
         Environment = "${var.environment}"
         Layer       = "${var.layer}"
         ensured_by  = "${var.automation}"
   }
 }
 
-resource    "aws_subnet"    "private_subnet_b"   {
-    vpc_id      =   "${module.aws_vpc.vpc_id}"
-    cidr_block  =   "10.0.20.0/24"
-
-    tags         {
-        Name        = "${var.vpc_private_subnet_name_b}"
-        Environment = "${var.environment}"
-        Layer       = "${var.layer}"
-        ensured_by  = "${var.automation}"
-  }
-}
-
-resource    "aws_subnet"    "private_subnet_c"   {
-    vpc_id      =   "${module.aws_vpc.vpc_id}"
-    cidr_block  =   "10.0.30.0/24"
-
-    tags         {
-        Name        = "${var.vpc_private_subnet_name_c}"
-        Environment = "${var.environment}"
-        Layer       = "${var.layer}"
-        ensured_by  = "${var.automation}"
-  }
-}
-
-resource    "aws_subnet"    "public_subnet_a"   {
-    vpc_id      =   "${module.aws_vpc.vpc_id}"
-    cidr_block  =   "10.0.100.0/24"
-
-    tags         {
-        Name        = "${var.vpc_public_subnet_name_a}"
-        Environment = "${var.environment}"
-        Layer       = "${var.layer}"
-        ensured_by  = "${var.automation}"
-  }
-}
-
-resource    "aws_subnet"    "public_subnet_b"   {
-    vpc_id      =   "${module.aws_vpc.vpc_id}"
-    cidr_block  =   "10.0.110.0/24"
-
-    tags         {
-        Name        = "${var.vpc_public_subnet_name_b}"
-        Environment = "${var.environment}"
-        Layer       = "${var.layer}"
-        ensured_by  = "${var.automation}"
-  }
-}
-
-resource    "aws_subnet"    "public_subnet_c"   {
-    vpc_id      =   "${module.aws_vpc.vpc_id}"
-    cidr_block  =   "10.0.120.0/24"
-
-    tags         {
-        Name        = "${var.vpc_public_subnet_name_c}"
-        Environment = "${var.environment}"
-        Layer       = "${var.layer}"
-        ensured_by  = "${var.automation}"
-  }
-}
+#resource    "aws_subnet"    "private_subnet_b"   {
+#    vpc_id      =   "${module.aws_vpc.vpc_id}"
+#    cidr_block  =   "10.0.20.0/24"
+#
+#    tags         {
+#        Name        = "${var.vpc_private_subnet_name_b}"
+#        Environment = "${var.environment}"
+#        Layer       = "${var.layer}"
+#        ensured_by  = "${var.automation}"
+#  }
+#}
+#
+#resource    "aws_subnet"    "private_subnet_c"   {
+#    vpc_id      =   "${module.aws_vpc.vpc_id}"
+#    cidr_block  =   "10.0.30.0/24"
+#
+#    tags         {
+#        Name        = "${var.vpc_private_subnet_name_c}"
+#        Environment = "${var.environment}"
+#        Layer       = "${var.layer}"
+#        ensured_by  = "${var.automation}"
+#  }
+#}
+#
+#resource    "aws_subnet"    "public_subnet_a"   {
+#    vpc_id      =   "${module.aws_vpc.vpc_id}"
+#    cidr_block  =   "10.0.100.0/24"
+#
+#    tags         {
+#        Name        = "${var.vpc_public_subnet_name_a}"
+#        Environment = "${var.environment}"
+#        Layer       = "${var.layer}"
+#        ensured_by  = "${var.automation}"
+#  }
+#}
+#
+#resource    "aws_subnet"    "public_subnet_b"   {
+#    vpc_id      =   "${module.aws_vpc.vpc_id}"
+#    cidr_block  =   "10.0.110.0/24"
+#
+#    tags         {
+#        Name        = "${var.vpc_public_subnet_name_b}"
+#        Environment = "${var.environment}"
+#        Layer       = "${var.layer}"
+#        ensured_by  = "${var.automation}"
+#  }
+#}
+#
+#resource    "aws_subnet"    "public_subnet_c"   {
+#    vpc_id      =   "${module.aws_vpc.vpc_id}"
+#    cidr_block  =   "10.0.120.0/24"
+#
+#    tags         {
+#        Name        = "${var.vpc_public_subnet_name_c}"
+#        Environment = "${var.environment}"
+#        Layer       = "${var.layer}"
+#        ensured_by  = "${var.automation}"
+#  }
+#}
